7f4ec75e1a12b4e8aad743510120d7caa7914b31
tanguypouriel <pourieltanguy@gmail.com>
2024-11-13 15:15:15 +0100

    Update about_me section

--- src/app/components/about_me_section.tsx
+++ src/app/components/about_me_section.tsx
@@ -1,5 +1,9 @@
 import { MapPin } from 'lucide-react';
 import Animation from "../components/animation";
+import Image from 'next/image';
+import flutterImage from '../../../public/flutter-app.png';
+import { Separator } from "@/components/ui/separator"
+
 
 interface AboutMeProps {
     className?: string;
@@ -10,11 +14,11 @@ export default function AboutMeSection({ className }: AboutMeProps) {
     return (
         <section className={`h-screen flex flex-col items-center p-6 ${className}`}>
 
-            <div className="w-full md:h-2/3 flex flex-col-reverse md:flex-row items-center p-8">
+            <div className="w-full md:h-2/5 flex flex-col-reverse md:flex-row items-center p-8">
                 <div className='flex-[5_5_0%] flex flex-col items-center '>
                     <div className='flex flex-col gap-4'>
                         <h2 className="text-4xl md:text-8xl font-bold text-indigo-50">Tanguy Pouriel</h2>
-                        <h2 className="text-md md:text-xl font-bold text-teal-100 mx-8">Fullstack Mobile developer</h2>
+                        <h2 className="text-md md:text-xl font-bold text-teal-100 mx-8">Fullstack Mobile developer - Expert Flutter</h2>
                         <div className='flex flex-row text-teal-50 mt-2'>
                             <MapPin className='mx-2 ml-8' />
                             Bordeaux, France
@@ -27,13 +31,37 @@ export default function AboutMeSection({ className }: AboutMeProps) {
                 </div>
             </div>
 
-
-            <div className='flex justify-center h-full'>
-                <p className="self-center justify-center text-center text-xl md:text-2xl px-2 md:px-8 text-justify md:mx-8">
-                    With an engineering degree from a top-tier school and three years of mobile development experience,
-                    I empower businesses and individuals to create high-performing, visually appealing and innovative apps. <br /><br />
-                    My expertise spans full-cycle Flutter app development, backend integration, and seamless deployment.
+            <div className='h-3/5'>
+                <p className="self-center justify-center text-center text-xl md:text-xl px-2 md:px-8 text-justify md:mx-8">
+                    Hi I&apos;m Tanguy ! A freelance developer based on Bordeaux. <br /> With an engineering degree from a top-tier school and 4 years in
+                    mobile development, I have a solid experience in mobile development!
                 </p>
+
+                <div className='flex items-center justify-center w-full'>
+                <Separator className='my-10 w-4/5' />
+                </div>
+
+                <div className='flex flex-row h-full mx-6'>
+                    <div className='flex-[7_7_0%] justify-center text-center text-xl md:text-xl px-2 text-justify md:mx-8 h-full pr-10 mt-4'>
+                        I use Flutter to build your app from the ground up or enhance your existing one, whether you&apos;re an individual or a business.
+                        <br /><br />My services include:
+                        <ul className='list-disc pl-8 pt-8 space-y-2'>
+                            <li>UI design and prototyping with Figma 🎨</li>
+                            <li>Maintenance of existing applications and bug resolution 🛠️</li>
+                            <li>Full development of Flutter applications, from idea to finished product 🚀</li>
+                            <li>Complete backend development with Firebase 🔥</li>
+                            <li>Publishing on stores (iOS, Android) 📲</li>
+                        </ul>
+                    </div>
+
+                    <Image
+                        src={flutterImage}
+                        alt="flutter image"
+                        className='flex-[1_1_0%] p-16 pb-48'
+                        quality={100}
+                    />
+                </div>
+
             </div>
         </section>
     );
